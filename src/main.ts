e6580374a91b870c41e9193ffc4b416d7577dfcd
Viktor Yakushenko <yakushenkovitek@gmail.com>
2023-06-27 01:50:53 +0500

    Add bootstrap vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVueNext } from "bootstrap-vue-next";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(BootstrapVueNext)
+.use(router)
+.mount('#app')
